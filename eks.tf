46b0e84a383846426037340290859accabe3d081
FelipeSaijo <felipesaijo@protonmail.com>
2023-04-12 10:24:06 -0300

    create VPC and EKS infraestructure

---
+++ eks.tf
@@ -0,0 +1,110 @@
+### EKS CLUSTER ###
+resource "aws_eks_cluster" "project_cluster" {
+  name                    = "FS-Project-Cluster"
+  role_arn                = aws_iam_role.eks_cluster_role.arn
+  
+  vpc_config {
+    subnet_ids  = [aws_subnet.public_subnet[0].id, aws_subnet.public_subnet[1].id]
+    endpoint_public_access  = true
+  }   
+  
+  depends_on = [
+    aws_iam_role_policy_attachment.EKS_Cluster_Policy,
+    aws_iam_role_policy_attachment.EKS_VPC_Resource_Controller,
+  ]
+}
+
+### IAM ROLE EKS CLUSTER ###
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"  
+  
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    } 
+  
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "EKS_Cluster_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "EKS_VPC_Resource_Controller" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+### EKS NODES ###
+resource "aws_eks_node_group" "cluster_node" {
+  cluster_name    = aws_eks_cluster.project_cluster.name
+  node_group_name = "nginx_node"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.public_subnet[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  } 
+
+  instance_types  = ["t2.small"]
+
+  update_config {
+    max_unavailable = 2
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.EKS-Worker-Node-Policy,
+    aws_iam_role_policy_attachment.EKS-CNI-Policy,
+    aws_iam_role_policy_attachment.EC2Container-Registry-ReadOnly,
+  ]
+}
+
+### IAM ROLE EKS NODES ###
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "EKS-Worker-Node-Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "EKS-CNI-Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "EC2Container-Registry-ReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+### OUTPUTS ###
+output "eks-cluster-endpoint" {
+  value = aws_eks_cluster.project_cluster.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.project_cluster.certificate_authority[0].data
+}
---
+++ vpc.tf
@@ -0,0 +1,148 @@
+### VPC ###
+resource "aws_vpc" "main" {
+    cidr_block           = "10.132.0.0/16"
+    instance_tenancy     = "default"
+    enable_dns_support   = true
+    enable_dns_hostnames = true
+
+    tags = {
+        Name = "VPC Project"
+    }
+}
+
+### PUBLIC SUBNETS ###
+resource "aws_subnet" "public_subnet" {
+    vpc_id                  = aws_vpc.main.id
+    count                   = length(var.public_subnets_cidrs)
+    cidr_block              = element(var.public_subnets_cidrs, count.index)
+    availability_zone       = element(var.availability_zones, count.index)
+    map_public_ip_on_launch = true
+
+    tags = {
+      Name = "Public-Subnet-${count.index + 1}"
+    }
+}
+
+### PRIVATE SUBNETS ###
+resource "aws_subnet" "private_subnet" {
+    vpc_id                  = aws_vpc.main.id
+    count                   = length(var.private_subnets_cidrs)
+    cidr_block              = element(var.private_subnets_cidrs, count.index)
+    availability_zone       = element(var.availability_zones, count.index)
+
+    tags = {
+      Name = "Private-Subnet-${count.index + 1}"
+    }
+}
+
+### SECURITY GROUP ###
+resource "aws_security_group" "allow_http" {
+    name        = "allow_http"
+    description = "Allow HTTP inbound traffic"
+    vpc_id      = aws_vpc.main.id   
+    
+    ingress {
+      description      = "HTTP from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }   
+    
+    egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }   
+    
+    tags = {
+      Name = "allow_http"
+    }
+}
+
+resource "aws_security_group" "allow_https" {
+    name        = "allow_https"
+    description = "Allow HTTPS inbound traffic"
+    vpc_id      = aws_vpc.main.id   
+    
+    ingress {
+      description      = "HTTPS from VPC"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }   
+    
+    egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }   
+    
+    tags = {
+      Name = "allow_https"
+    }
+}
+
+resource "aws_security_group" "allow_ssh" {
+    name        = "allow_ssh"
+    description = "Allow SSH inbound traffic"
+    vpc_id      = aws_vpc.main.id   
+    
+    ingress {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }   
+    
+    egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }   
+    
+    tags = {
+      Name = "allow_ssh"
+    }
+}
+
+### INTERNET GATEWAY ###
+resource "aws_internet_gateway" "internet_gateway" {
+    vpc_id = aws_vpc.main.id    
+    
+    tags = {
+      Name = "Main Internet Gateway"
+    }
+}
+
+### PUBLIC ROUTE TABLE ###
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.main.id    
+    
+    tags = {
+      Name = "Public Route Table"
+    }
+}
+
+### PUBLIC ROUTE ###
+resource "aws_route" "public_route" {
+    route_table_id         = aws_route_table.public_rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id             = aws_internet_gateway.internet_gateway.id
+    depends_on             = [aws_route_table.public_rt]
+}
+
+### PUBLIC ROUTE TABLE ASSOCIATION ###
+resource "aws_route_table_association" "public_rt_association" {
+    count = length(var.public_subnets_cidrs)
+    subnet_id      = element(aws_subnet.private_subnet[*].id, count.index)
+    route_table_id = aws_route_table.public_rt.id
+}
